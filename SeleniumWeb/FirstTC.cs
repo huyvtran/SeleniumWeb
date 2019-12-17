762578af37a74372b5d312e1fd59fa5f74af5e94
artemKvashnin <artem.kvashnin@murka.com>
2019-12-17 20:58:56 +0200

    firstTest

--- SeleniumWeb/FirstTC.cs
+++ SeleniumWeb/FirstTC.cs
@@ -8,10 +8,15 @@ namespace SeleniumWeb
     class FirstTC
     {
         [Test]
-        public void Test()
+        public  void Test()
         {
             IWebDriver driver = new ChromeDriver();
             driver.Url = "http://google.com";
+            driver.FindElement(By.Name("q")).SendKeys("five");
+            driver.FindElement(By.Name("q")).Click();
+            driver.FindElement(By.Name("btnK"));
+            var path = "//*[@id='rhs']//div[2]/div[1]/span";
+            Assert.That(false, path);
             driver.Quit();
         }
     }
